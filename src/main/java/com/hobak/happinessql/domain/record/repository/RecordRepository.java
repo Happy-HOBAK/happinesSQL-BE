65b6fab82582e7c8900c4747973ee4d69499c55c
Jung Ye Lim <101961939+yel-m@users.noreply.github.com>
2024-03-27 18:52:07 +0900

    Revert "[#5] 에러 헨들러 세팅"

--- src/main/java/com/hobak/happinessql/domain/record/repository/RecordRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hobak.happinessql.domain.record.repository;
-
-import com.hobak.happinessql.domain.record.domain.Record;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecordRepository extends JpaRepository<Record, Long> {
-}
--- src/main/java/com/hobak/happinessql/domain/user/repository/UserRespository.java
+++
@@ -1,7 +0,0 @@
-package com.hobak.happinessql.domain.user.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.User;
-
-public interface UserRespository extends JpaRepository<User, Long> {
-}
